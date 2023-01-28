9b2567ba3a5f180830d19ac62fe4616c7359841e
Wint War Khing <e1045763@u.nus.edu>
2023-01-29 02:01:07 +0800

    Mabel adduserForm

--- src/main/java/sg/edu/nus/iss/AD_Locum_Doctors/controllers/UserController.java
+++ src/main/java/sg/edu/nus/iss/AD_Locum_Doctors/controllers/UserController.java
@@ -5,9 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 import sg.edu.nus.iss.AD_Locum_Doctors.model.User;
+import sg.edu.nus.iss.AD_Locum_Doctors.service.OrganizationService;
 import sg.edu.nus.iss.AD_Locum_Doctors.service.UserService;
 
 @Controller
@@ -16,12 +21,30 @@ public class UserController {
     @Autowired
     UserService uService;
 
+    @Autowired
+    OrganizationService oService;
+
     @GetMapping("/UserList")
     public String userListPage(Model model){
         List<User> users = uService.findAll();
         model.addAttribute("userList", users);
         return "user-list";
     }
+
+    @GetMapping("/addUserForm")
+    public String addUserForm(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("organizationList", oService.getAllOrganizations());
+        return "addUserForm";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveClinic(@ModelAttribute("user") @Valid User user, BindingResult result,
+   Model model){
+    uService.saveUser(user);
+    return "redirect:/UserList";
+   }
 
     
 }
