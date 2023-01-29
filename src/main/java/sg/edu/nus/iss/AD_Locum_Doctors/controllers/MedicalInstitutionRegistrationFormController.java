950b77eeaceba7dfb5e18931ed8d42303ff838ea
Wint War Khing <e1045763@u.nus.edu>
2023-01-29 16:42:33 +0800

    Mabel Create admin when organization register

--- src/main/java/sg/edu/nus/iss/AD_Locum_Doctors/controllers/MedicalInstitutionRegistrationFormController.java
+++ src/main/java/sg/edu/nus/iss/AD_Locum_Doctors/controllers/MedicalInstitutionRegistrationFormController.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss.AD_Locum_Doctors.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,20 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.validation.Valid;
 import sg.edu.nus.iss.AD_Locum_Doctors.model.Organization;
+import sg.edu.nus.iss.AD_Locum_Doctors.model.User;
 import sg.edu.nus.iss.AD_Locum_Doctors.service.OrganizationService;
+import sg.edu.nus.iss.AD_Locum_Doctors.service.UserService;
 
 @Controller
 public class MedicalInstitutionRegistrationFormController {
-    
+
     @Autowired
     OrganizationService orgService;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/medicalInstitutionRegistrationForm")
-    public String medicalInstitutionRegistrationForm(Model model){
+    public String medicalInstitutionRegistrationForm(Model model) {
         Organization organization = new Organization();
         model.addAttribute("organization", organization);
         return "medicalInstitutionRegistrationForm";
@@ -28,8 +37,25 @@ public class MedicalInstitutionRegistrationFormController {
 
     @PostMapping("/registerOrg")
     public String registerOrg(@ModelAttribute("organization") @Valid Organization organization, BindingResult result,
-   Model model){
-    orgService.CreateOrganization(organization);
-    return "";
-   }
+            Model model) {
+        orgService.CreateOrganization(organization);
+        return "redirect:/registerAdmin/" + organization.getId();
+    }
+
+    @GetMapping("/registerAdmin/{id}")
+    public String registerAdmin(Model model, @PathVariable(value = "id") Long id) {
+        Organization organization = orgService.findById(id);
+        List<Organization> organizations = new ArrayList();
+        organizations.add(organization);
+        model.addAttribute("user", new User());
+        model.addAttribute("organizationList", organizations);
+        return "registerAdminForm";
+    }
+
+    @PostMapping("/createAdmin")
+    public String createAdmin(@ModelAttribute("user") @Valid User user, BindingResult result,
+            Model model) {
+        userService.saveUser(user);
+        return "redirect:/login";
+    }
 }
